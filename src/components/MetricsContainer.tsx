f004b8e038843706666dc7744053173a0b30e243
losajjang <losajjang@naver.com>
2022-06-26 22:14:25 +0900

    feat: 각 컴포넌트 뷰 생성

---
+++ src/components/MetricsContainer.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+const MetricsContainer = () => {
+  return (
+    <MetricBox>
+      <MetricItem>
+        <strong>
+          <span>350</span>만 명
+        </strong>
+        의 사용자
+      </MetricItem>
+      <MetricItem>
+        <strong>
+          <span>21</span>만 개
+        </strong>
+        의 리뷰
+      </MetricItem>
+      <MetricItem>
+        <strong>
+          <span>650</span>만 개
+        </strong>
+        의 저장
+      </MetricItem>
+    </MetricBox>
+  );
+};
+
+const MetricBox = styled.div`
+`;
+
+const MetricItem = styled.div`
+  margin-bottom: 20px;
+  font-family: sans-serif;
+  font-size: 36px;
+  color: #3a3a3a;
+  letter-spacing: -1px;
+
+  &:last-child {
+    margin-bottom: 0px;
+  }
+`;
+
+export default MetricsContainer;
